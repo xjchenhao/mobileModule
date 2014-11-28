6e6cbe272adb08e204dad72e53819c372a371506
chenhao <wy.chenhao@qq.com>
2014-11-28 15:36:06 +0800

    基本的模态弹出——alert、loading、pop

--- src/module/pop.js
+++ src/module/pop.js
@@ -13,13 +13,18 @@
         this._setting();
         this._pop();
     };
+    /*初始设置*/
     Pop.prototype._setting = function () {
-        var opts = this._opts || {};
+        var opts = this._opts || {},
+            self = this,
+            $ = root.$ || root.jQuery || root.Zepto;
         this.type = opts.type || 'alert';
         this.content = opts.content;
         this.additional = opts.additional || '';
         this.width = opts.width || 'auto';
         this.height = opts.height || 'auto';
+        this.callback = opts.callback || '';
+        /*样式*/
         this.class = {
             mask: opts.clsMask || '#pop-mask',
             con: opts.clsCon || '#pop-content',
@@ -27,74 +32,112 @@
             inio: opts.clsInio || '.pop-inio',
             out: opts.clsOut || '.pop-out'
         };
-    };
-    Pop.prototype._addDom = function () {
-        var maskLayer = document.createElement('div');
-        switch (this.class.mask.slice(0, 1)) {
-            case '.' :
-                maskLayer.setAttribute('class', this.class.mask.slice(1));
-                break;
-            case '#':
-                maskLayer.setAttribute('id', this.class.mask.slice(1));
-                break;
-        }
-        var closeBtn = document.createElement('a');
-        closeBtn.setAttribute('href', 'javascript:;');
-        switch (this.class.close.slice(0, 1)) {
-            case '.' :
-                closeBtn.setAttribute('class', this.class.close.slice(1));
-                break;
-            case '#':
-                closeBtn.setAttribute('id', this.class.close.slice(1));
-                break;
-        }
-        var box = document.createElement('div');
-        switch (this.class.con.slice(0, 1)) {
-            case '.' :
-                box.setAttribute('class', this.class.con.slice(1));
-                break;
-            case '#':
-                box.setAttribute('id', this.class.con.slice(1));
-                break;
-        }
-        box.appendChild(closeBtn);
-        var content = document.createElement('div');
-        content.setAttribute('class', 'content');
-        box.appendChild(content);
-        document.getElementsByTagName('body')[0].appendChild(box);
-        document.getElementsByTagName('body')[0].appendChild(maskLayer);
-    };
-    Pop.prototype._pop = function () {
-        this._addDom();
-        var self = this,
-            box = document.querySelector(this.class.con),
-            closeBtn = document.querySelector(this.class.close),
-            maskLayer = document.querySelector(this.class.mask),
-            boxHeight = 0,
-            boxWidth = 0;
+        /*事件名称*/
+        this.touch = {
+            tap: $ && $.fn && $.fn.tap ? 'tap' : 'click'
+        };
+        /*事件集合*/
         this.event = {
             close: function () {
-                closeBtn.removeEventListener('click', self.event.close, false);
+                var box = document.querySelector(self.class.con),           //pop容器
+                    closeBtn = document.querySelector(self.class.close),    //关闭按钮
+                    maskLayer = document.querySelector(self.class.mask);    //遮罩层
+                closeBtn && closeBtn.removeEventListener(self.touch.tap, self.event.close, false);
                 box.remove();
                 maskLayer.remove();
+            },
+            addDomMask: function () {
+                var maskLayer = document.createElement('div');
+                switch (self.class.mask.slice(0, 1)) {
+                    case '.' :
+                        maskLayer.setAttribute('class', self.class.mask.slice(1));
+                        break;
+                    case '#':
+                        maskLayer.setAttribute('id', self.class.mask.slice(1));
+                        break;
+                }
+                document.getElementsByTagName('body')[0].appendChild(maskLayer);
+                return maskLayer;
+            },
+            addDomBox: function () {
+                var box = document.createElement('div');
+                switch (self.class.con.slice(0, 1)) {
+                    case '.' :
+                        box.setAttribute('class', self.class.con.slice(1));
+                        break;
+                    case '#':
+                        box.setAttribute('id', self.class.con.slice(1));
+                        break;
+                }
+                var content = document.createElement('div');
+                content.setAttribute('class', 'content');
+                box.appendChild(content);
+                document.getElementsByTagName('body')[0].appendChild(box);
+                return box;
+            },
+            addDomCloseBtn: function () {
+                var box = document.querySelector(self.class.con);           //pop容器
+                var closeBtn = document.createElement('a');
+                closeBtn.setAttribute('href', 'javascript:;');
+                switch (self.class.close.slice(0, 1)) {
+                    case '.' :
+                        closeBtn.setAttribute('class', self.class.close.slice(1));
+                        break;
+                    case '#':
+                        closeBtn.setAttribute('id', self.class.close.slice(1));
+                        break;
+                }
+                box.appendChild(closeBtn);
+                return closeBtn;
             }
         };
+    };
+    /*主逻辑*/
+    Pop.prototype._pop = function () {
+        var self = this,
+            box = null,          //pop容器
+            maskLayer = null,    //遮罩层
+            closeBtn = null,     //关闭按钮
+            boxHeight = 0,
+            boxWidth = 0;
+        /*分类执行*/
         switch (this.type) {
             case 'alert' :
+                maskLayer = self.event.addDomMask();
+                box = self.event.addDomBox();
+                closeBtn = self.event.addDomCloseBtn();
+                box.style['padding'] = '10px';
                 box.getElementsByClassName('content')[0].innerHTML = this.content;
-                closeBtn.addEventListener('click', self.event.close, false);
+                closeBtn.addEventListener(self.touch.tap, self.event.close, false);
                 break;
             case 'loading' :
-
+                maskLayer = self.event.addDomMask();
+                box = self.event.addDomBox();
+                box.getElementsByClassName('content')[0].innerHTML = this.content;
+                box.style['font'] = '0/0 a';
+                box.getElementsByClassName('content')[0].style.cssText = 'text-align:center;line-height:' + box.clientHeight + 'px';
+                box.getElementsByTagName('img')[0].style['vertical-align'] = 'middle';
+                this.callback() && self.event.close();
+                break;
+            case 'pop' :
+                maskLayer = self.event.addDomMask();
+                box = self.event.addDomBox();
+                box.style['background-color'] = 'initial';
+                box.getElementsByClassName('content')[0].innerHTML = this.content;
                 break;
             default :
                 throw new Error("不支持的类型!");
         }
+        /*居中对齐*/
         boxHeight = box.clientHeight;
         boxWidth = box.clientWidth;
         box.style['margin-left'] = -boxWidth / 2 + 'px';
         box.style['margin-top'] = -boxHeight / 2 + 'px';
+        /*执行追加函数*/
         this.additional && this.additional();
+    };
+    Pop.prototype.close = function () {
+        this.event.close();
     };
     return Pop;
 });
