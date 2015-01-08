c1416142e11edc9fe067eade7bec4b563ebf846a
chenhao <wy.chenhao@qq.com>
2015-01-08 11:13:54 +0800

    增加api，是否添加美元逗号

--- src/module/run-number.js
+++ src/module/run-number.js
@@ -1,5 +1,5 @@
 /**
- * 数字跑动      1.0.1
+ * 数字跑动      1.0.2
  * eg:
  * <div id="number" data-val="43456.4433"></div>
  *
@@ -9,6 +9,7 @@
  *      step: 19,          //分成几步完成
  *      decimal: 3,       //保留小数点后几位（包含在span标签内）
  *      speed: 50         //滚动速度
+ *      isCommas || false;   //是否添加美元逗号
  *  });
  *  //运行跑动
  *  oRunNumber.run({
@@ -42,6 +43,7 @@
         this.step = opts.step || 19;          //分成几步完成
         this.decimal = opts.decimal || 0;     //保留小数点后几位（包含在span标签内）
         this.speed=opts.speed || 50;          //滚动速度
+        this.isCommas=opts.isCommas || false;   //是否添加美元逗号
     };
     runNumber.prototype.run=function(opts){
         var self=this,
@@ -49,31 +51,44 @@
             val=obj.getAttribute('data-val'),
             arr=[],
             page=0;
-        this.domArr.push(obj);
+        this.domArr.push(obj);      //存储被添加数字跑动的dom
         for(var i=self.step;i>=0;i--){
-            arr.push((Number(val)/i).toFixed(self.decimal)+"");
+            arr.push(this._addArr(Number(val)/i));
         }
         function roll(){
-            var reg=new RegExp('([0-9]+).([0-9]{'+self.decimal+'})[0-9]*'),
-                integer=arr[page].replace(reg,"$1"),
-                decimal=arr[page].replace(reg,"$2");
             if(page<self.step){
-                if(self.decimal){
-                    obj.innerHTML=integer+'.<span>'+decimal+'</span>';
-                }else{
-                    obj.innerHTML=integer;
-                }
+                obj.innerHTML=arr[page];
                 page++;
                 setTimeout(roll,self.speed);
             }else{
-                if(self.decimal){
-                    obj.innerHTML=val.replace(reg,"$1")+'.<span>'+val.replace(reg,"$2")+'</span>';
-                }else{
-                    obj.innerHTML=val.replace(reg,"$1");
-                }
+                obj.innerHTML=self.isCommas ? self._addCommas(val) : val;
             }
         }
         setTimeout(roll,self.speed);
+    };
+    runNumber.prototype._addArr=function(_val){
+        var self=this,
+            arrVal=_val.toFixed(self.decimal)+"",
+            reg=new RegExp('([0-9]+[,0-9]*)\\.([0-9]{'+self.decimal+'})[0-9]*'),
+            str=self.isCommas ? self._addCommas(arrVal) : arrVal,
+            integer=str.replace(reg,"$1"),
+            decimal=str.replace(reg,"$2");
+        if(self.decimal){
+            return integer+'.<span>'+decimal+'</span>';
+        }else{
+            return integer;
+        }
+    };
+    runNumber.prototype._addCommas=function(nStr){
+        nStr += ''; //转换字符串
+        var x = nStr.split('.');
+        var x1 = x[0];
+        var x2 = x.length > 1 ? '.' + x[1] : '';
+        var rgx = /(\d+)(\d{3})/;
+        while (rgx.test(x1)) {
+            x1 = x1.replace(rgx, '$1' + ',' + '$2');
+        }
+        return x1 + x2;
     };
     runNumber.prototype.destroy = function () {
         roll=null;
