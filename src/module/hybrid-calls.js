9f0d7acbb0bf00bce3f6adf376b9f33201e46caa
chenhao <wy.chenhao@qq.com>
2015-05-21 15:59:54 +0800

    增加原生接收协议后成功和失败的回调

--- src/module/hybrid-calls.js
+++ src/module/hybrid-calls.js
@@ -1,8 +1,8 @@
 /**
- * Hybrid交互      1.0.3
+ * Hybrid交互      1.0.5
  * 此模块用来与原生app进行交互
  * 暴露出去一个方法，调用方式如下：
- * requestHybrid({
+ * hybridProtocol({
  *     tagName: 'getUrl',
  *     data: {
  *         url: 'ensure.html'
@@ -12,17 +12,22 @@
  *     }
  * });
  * 最后生成与原生通信的url并发送请求：`qian://submit?callback=hybrid_1427422528529&url=ensure.html`
- * 原生那边根据这个url，截取前面的tagName知道要做什么，然后再去拿后面需要的参数。注意：参数中的hybrid_1427422528529是暴露给原生的函数，以供原生调用网页函数，调用方式为window.Hybrid[hybrid_1427422528529]。（它是根据调用时的callback自动生成的函数名，并带上了时间戳）
+ * 原生那边根据这个url，截取前面的tagName知道要做什么，然后再去拿后面需要的参数。注意：参数中的hybrid_1427422528529是暴露给原生的函数，以供原生调用网页函数，调用方式为root.Hybrid[hybrid_1427422528529]。（它是根据调用时的callback自动生成的函数名，并带上了时间戳）
  */
 (function (root, factory) {
+    if (document.HYLOADED) { return; }
     if (typeof define === 'function' && (define.amd || define.cmd)) {
         define(function (exports) {
             return factory(root, exports);
         });
     } else {
-        root.requestHybrid = factory(root, {});
+        root.hybridProtocol =root.requestHybrid= factory(root, {});
     }
-})(this, function (root, hybrid) {
+})(this, function (root, exports) {
+    if (document.HYLOADED) {
+        return 'Don\'t repeat load hybridProtocol!';
+    }
+
     //判断浏览器环境
     var browser = {
         versions: function () {
@@ -35,63 +40,71 @@
     };
 
     //暴露给原生的对象
-    window.Hybrid = {};
+    root.Hybrid = {};
 
     //页面级用户调用的方法
-    var requestHybrid = function (params) {
-        //生成唯一执行函数，执行后销毁
-        var t = 'hybrid_' + (new Date().getTime());
-        //处理有回调的情况
-        if (params.callback) {
-            params.callbackName = t;
-            window.Hybrid[t] = function (data) {
-                params.callback(data);
-                delete window.Hybrid[t];
-            }
-        }
-
-        this.bridgePostMessage(this._getHybridUrl(params))
+    var hybridProtocol = function (obj) {
+        this._params=obj;
+        this._getHybridUrl();
+        this._bridgePostMessage();
     };
-    //封装统一的发送url接口，解决ios、android兼容问题，这里发出的url会被拦截，会获取其中参数，比如：
-    //这里会获取getAdressList参数，调用native接口回去通讯录数据，形成json data数据，拿到webview的window执行，window.Hybrid['hybrid_1427422528529'](data)
-    requestHybrid.prototype.bridgePostMessage = function (url) {
-        //if (browser.versions.ios) {
-        //    window.location = url;
-        //}
-        //if (browser.versions.android) {
-        var ifr = document.createElement("iframe");
+
+    hybridProtocol.prototype._bridgePostMessage = function () {
+        var self=this,
+            ifr = document.createElement("iframe");
         ifr.setAttribute('id', 'hybridRequest');
-        ifr.setAttribute('src', url);
+        ifr.setAttribute('src', self.url);
         document.getElementsByTagName("body")[0].appendChild(ifr);
         ifr.parentNode.removeChild(ifr);
-        //}
+        if(self._params.success){
+            self._overtime=setTimeout(function(){
+                self._params.error();
+            },3000);
+        }
     };
 
-    //根据参数返回满足Hybrid条件的url，比如qian://getAdressList?callback=hybrid_1427422528529
-    requestHybrid.prototype._getHybridUrl = function (params) {
-        var url = '';
-        url += 'qian://' + params.tagName + '?';
-        if (params.callback) {
-            url += 'callback=' + params.callbackName + '&';
-        }
-        if (params.data) {
-            for (x in params.data) {
-                url += x + '=' + encodeURIComponent(params.data[x]) + '&';
+    //生成协议url
+    hybridProtocol.prototype._getHybridUrl = function () {
+        var self=this,
+            url = 'qian://' + self._params.tagName + '?',
+            timeStamp=new Date().getTime(),
+            addCallback=function(str){
+                var callbackName=str+timeStamp;
+                root.Hybrid[callbackName] = function (data) {
+                    self._params[str](data);
+                    clearTimeout(self._overtime);
+                    delete root.Hybrid[callbackName];
+                };
+                url += str + '=' + encodeURIComponent(callbackName) + '&';
+            };
+        for (var params in self._params.data) {
+            //处理有回调的情况
+            if (params =='callback') {
+                var callbackName = 'hybrid_' + timeStamp;
+                root.Hybrid[callbackName] = function (data) {
+                    params.callback(data);
+                    delete root.Hybrid[callbackName];
+                };
+                url += params + '=' + encodeURIComponent(callbackName) + '&';
+            }else{
+                url += params + '=' + encodeURIComponent(self._params.data[params]) + '&';
             }
         }
+        self._params.success && addCallback('success');
+        self._params.error && addCallback('error');
+
         url = url.replace(/^(.+)&$/ig, '$1');
-        return url;
+        self.url=url;
     };
 
 
+    document.HYLOADED = true;
 
-    //模块暴露requestHybrid函数
     return function(obj){
-        //确保该函数作为构造函数被调用
         if(!(this instanceof requestHybrid)){
-            return new requestHybrid(obj);
+            return new hybridProtocol(obj);
         }else{
-            return requestHybrid;
+            return hybridProtocol;
         }
     };
 });
